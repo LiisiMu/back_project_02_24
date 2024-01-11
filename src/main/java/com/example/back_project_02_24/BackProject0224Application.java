f474fedad0a62276de2399d69e1f5feb5e355484
LiisiM <liisi.muur@gmail.comm>
2024-01-11 10:58:34 +0200

    Initial commit

---
+++ src/main/java/com/example/back_project_02_24/BackProject0224Application.java
@@ -0,0 +1,13 @@
+package com.example.back_project_02_24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackProject0224Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackProject0224Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/back_project_02_24/BackProject0224ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.back_project_02_24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackProject0224ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
